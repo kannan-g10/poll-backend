9332e0de68dcdfcc1a960693539880d89143935b
Kannan G <gkannan271120@gmail.com>
2024-08-08 10:19:32 +0530

    cors fix

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
-const connectDb = require('./utils/dbConnection');
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const connectDb = require("./utils/dbConnection");
 
-const pollRouter = require('./routes/pollRouter');
-const commentsRouter = require('./routes/commentsRouter');
+const pollRouter = require("./routes/pollRouter");
+const commentsRouter = require("./routes/commentsRouter");
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors());
 
 connectDb();
 
-app.use('/api/v1/polls', pollRouter);
-app.use('/api/v1/comments', commentsRouter);
+app.use("/api/v1/polls", pollRouter);
+app.use("/api/v1/comments", commentsRouter);
 
-process.on('uncaughtException', error => {
+process.on("uncaughtException", (error) => {
   console.log(error.name, error.message);
-  console.log('Uncaught exception occurred. Shutting down!');
+  console.log("Uncaught exception occurred. Shutting down!");
 
   process.exit(1);
 });
 
-process.on('unhandledRejection', error => {
-  console.log('Unhandled Rejection:', error.name, error.message);
-  console.log('Unhandled rejection occurred. Shutting down!');
+process.on("unhandledRejection", (error) => {
+  console.log("Unhandled Rejection:", error.name, error.message);
+  console.log("Unhandled rejection occurred. Shutting down!");
   server.close(() => {
     process.exit(1);
   });
